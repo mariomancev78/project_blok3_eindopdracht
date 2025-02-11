5a32c6269072a111381ea6344a55003d4213b4ae
Mario <mariomancev78@gmail.com>
2025-02-11 11:43:53 +0100

    initial commit week_2, commit voor merge week1

---
+++ week_2/discord_bot.py
@@ -0,0 +1,30 @@
+import discord
+import dotenv
+
+
+# laden van env variables
+DISCORD_BOT_ID = dotenv.get_key(".env", "DISCORD_BOT_ID")
+DISCORD_SERVER_NAME = dotenv.get_key(".env", "DISCORD_SERVER_NAME")
+print(DISCORD_BOT_ID)
+print(DISCORD_SERVER_NAME)
+
+
+def main():
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        for guild in client.guilds:
+            if guild.name == DISCORD_SERVER_NAME:
+                break
+        print(f"Client: {client.user}\n Guild: {
+              guild.id}, Guild Name: {guild.name}")
+
+    client.run(DISCORD_BOT_ID)
+
+
+if __name__ == '__main__':
+    main()
