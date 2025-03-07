fb293e9c0e5ebd999366818860da56cbc7e7b0b5
Mario <mariomancev78@gmail.com>
2025-03-07 15:00:10 +0100

    Fix: main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import click
+from week_1.disable_defender import disable_defender, show_running_processes, show_banner, check_defender_status
+from week_2.discord_bot import start_bot
+from week_3.screenshot import take_screenshot
+
+
 @click.command()
 @click.option("--action", help="De actie die uitgevoerd moet worden.", type=click.Choice(
     ["disable_defender", "ps", "check_defender_status", "start_discord_bot", "take_screenshot"]))
@@ -11,19 +17,21 @@ def main(action):
             if check_os():
                 click.echo(disable_defender())
             else:
-                click.echo(f"Deze functie werkt alleen in een Windows omgeving. Jouw OS: {sys.platform}")
+                click.echo(f"Deze functie werkt alleen in een Windows omgeving. Jouw OS: {
+                           sys.platform}")
         case "ps":
             click.echo(show_running_processes())
         case "check_defender_status":
             if check_os():
                 click.echo(check_defender_status())
             else:
-                click.echo(f"Deze functie werkt alleen in een Windows omgeving. Jouw OS: {sys.platform}")
+                click.echo(f"Deze functie werkt alleen in een Windows omgeving. Jouw OS: {
+                           sys.platform}")
         case "start_discord_bot":
             start_bot()
         case "take_screenshot":
-            take_screenshot()  # Call the PowerShell screenshot function
+            take_screenshot()
+
 
 if __name__ == '__main__':
     main()
-
